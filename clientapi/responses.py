3600a5248a083ef47ee48c2f84c3a4edd44d4092
GonzaloSaad <gonzalo.saad@electric.ai>
2021-04-07 12:48:50 -0300

    Rename ElectricAPIErrorResponse to JsonAPIErrorResponse

--- clientapi/responses.py
+++ clientapi/responses.py
@@ -4,7 +4,22 @@ import pydantic
 from pydantic import BaseModel
 
 
-class ElectricAPIErrorResponse(BaseModel):
+class JsonAPIErrorResponse(BaseModel):
+    """
+    Schema to model a JsonAPI styled response
+
+    It only models a subset of the recommended attributes. Also, is good to consider
+    that this schema is used as a top level one, not wrapped up in `errors` as the spec
+    recommends.
+
+        "Error objects MUST be returned as an array keyed by errors in the top level of a JSON:API document"
+
+    Future version of this library will try to include that, but so far in this current version
+    there is no need to support that.
+
+    Docs:
+    https://jsonapi.org/format/#error-objects
+    """
     code: str
     detail: str
     source: Optional[Any]
