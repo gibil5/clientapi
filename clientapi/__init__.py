7fee38633595c672ca3caddb99eb9679edb737e2
GonzaloSaad <gonzalo.saad@electric.ai>
2021-05-20 15:09:20 -0300

    Add ContentType to package init

--- clientapi/__init__.py
+++ clientapi/__init__.py
@@ -1,11 +1,12 @@
 from . import responses
-from .client import ClientAPI
+from .client import ClientAPI, ContentType
 from .exceptions import APIClientError, APIHTTPError, APIValidationError
 from .parsers import parse
 
 __all__ = [
     "responses",
     "ClientAPI",
+    "ContentType",
     "APIClientError",
     "APIHTTPError",
     "APIValidationError",
